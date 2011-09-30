b3314095a293776b708c94e37ad9d14e784b7de1
tmesquita <tmesquit@mines.edu>
2011-09-30 10:16:47 -0600

    Intial EmployTown commit

---
+++ EmployTown/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EmployTown::Application.config.secret_token = 'b31f717934e244cdfe76e966fbcf23f0e9c21903d82ad4ea8ef8f49df357e9ec42c1311fd5bf42c0ea07d235ccbd31562ddfe4a000d91cb53feaf6a4d94f6447'
