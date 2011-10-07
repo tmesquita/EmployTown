15977bbf2cc58786c03d5ad60b4830eb570a7c68
Jerel Miller <jermille@mines.edu>
2011-10-06 21:11:11 -0600

    Adding role assignment

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
-      redirect_to root_url, :notice => "Signed up!"
+      flash[:notice] = "Signed up!"
+      redirect_to root_url
     else
       render :new
     end
