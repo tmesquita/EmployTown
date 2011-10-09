c4a30cbd15ac2dbd5338edcf1f7d0d74d2a09bf9
Thomas Mesquita <tmesquit@mines.edu>
2011-10-09 16:01:55 -0600

    forgot to add employers_seekers controller

---
+++ app/controllers/employers/seekers_controller.rb
@@ -0,0 +1,15 @@
+class Employers::SeekersController < Employers::EmployersController
+  def index
+  	@seekers = User.find(:all)
+  end
+
+  def show
+    @seeker = User.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.xml  { render :xml => @seeker }
+    end
+  end
+
+end
