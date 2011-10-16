203d8150af641cfb3e927111297ea4871043f6b6
Chad McKenna <chad.mckenna@gmail.com>
2011-10-16 13:27:08 -0600

    Testing search fix on Heroku

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -7,6 +7,10 @@ class Tag < ActiveRecord::Base
 
   def self.search(search)
       search.upcase
-      find(:all, :conditions => ['tag LIKE UPPER(?)', "%#{search}%"])
+      if ActiveRecord::Base.connection.instance_variable_get(:@config)[:database].split('/').last.eql? "development.sqlite3"
+        find(:all, :conditions => ['tag LIKE UPPER(?)', "%#{search}%"])
+      else
+        find(:all, :conditions => ['tag ILIKE UPPER(?)', "%#{search}%"])
+      end
   end
 end
