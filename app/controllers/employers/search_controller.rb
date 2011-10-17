e057e33609423f9cfbd0838b51610d95bf8322ac
Thomas Mesquita <tmesquit@mines.edu>
2011-10-17 15:09:33 -0600

    fixed duplicated search results

--- app/controllers/employers/search_controller.rb
+++ app/controllers/employers/search_controller.rb
@@ -4,8 +4,16 @@ class Employers::SearchController < Employers::EmployersController
   		puts params[:search]
   		@users = User.search(params[:search])
   		@users.delete_if{|user| user.seeking.eql? "employer"}
-  		@tags = Tag.search(params[:search])
+  		@tags = Tag.search(params[:search]).remove_duplicates_by{|tag| tag.user_id}
+  		#@tags = @tags
   	end
   end
 
+end
+
+class Array
+  def remove_duplicates_by
+    seen = Set.new
+    select{ |x| seen.add? (yield(x))}
+  end
 end
