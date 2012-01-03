6810acdc70af13a1c5cd2133010e82d425a6cf64
Chad McKenna <chad.mckenna@gmail.com>
2012-01-03 09:38:13 -0700

    Some search changes. Haven't finished, stashing for potential pull

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -6,12 +6,28 @@ class Tag < ActiveRecord::Base
   end
 
   def self.search(search)
-      search.upcase
-      if ActiveRecord::Base.connection.instance_variable_get(:@config)[:database].split('/').last.eql? "development.sqlite3"
-        find(:all, :conditions => ['tag LIKE UPPER(?)', "%#{search}%"])
+    search_string = "\"#{search.upcase}\""
+    search = search.split(" ")
+    if search.length > 1
+      search_string += " OR "
+      i = 1
+      search.each do |item|
+      if search.length.eql? i
+      search_string += "\"#{item.upcase}\""
       else
-        find(:all, :conditions => ['tag ILIKE UPPER(?)', "%#{search}%"])
+      search_string += "\"#{item.upcase}\" OR "
       end
+      i += 1
+      end
+    end
+    
+    puts search_string
+
+    if ActiveRecord::Base.connection.instance_variable_get(:@config)[:database].split('/').last.eql? "development.sqlite3"
+      find(:all, :conditions => ['tag LIKE UPPER(?)', "%#{search_string}%"])
+    else
+      find(:all, :conditions => ['tag ILIKE UPPER(?)', "%#{search_string}%"])
+    end
   end
   
   protected
