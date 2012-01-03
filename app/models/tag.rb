933a58c4ff60f6fd7fd8cce95012c1891c137931
Chad McKenna <chad.mckenna@gmail.com>
2012-01-03 13:09:34 -0700

    Search changes

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -6,27 +6,29 @@ class Tag < ActiveRecord::Base
   end
 
   def self.search(search)
-    search_string = "\"#{search.upcase}\""
+    
+    ##################################################################
+    # In progress
+    search_string = "tag LIKE UPPER(\"#{search.upcase}\")"
     search = search.split(" ")
     if search.length > 1
       search_string += " OR "
       i = 1
       search.each do |item|
       if search.length.eql? i
-      search_string += "\"#{item.upcase}\""
+      search_string += "tag LIKE UPPER(\"#{item.upcase}\")"
       else
-      search_string += "\"#{item.upcase}\" OR "
+      search_string += "tag LIKE UPPER(\"#{item.upcase}\") OR "
       end
       i += 1
       end
     end
-    
-    puts search_string
+    ##################################################################
 
     if ActiveRecord::Base.connection.instance_variable_get(:@config)[:database].split('/').last.eql? "development.sqlite3"
-      find(:all, :conditions => ['tag LIKE UPPER(?)', "%#{search_string}%"])
+      find(:all, :conditions => ['tag LIKE UPPER(?)', "%#{search}"])
     else
-      find(:all, :conditions => ['tag ILIKE UPPER(?)', "%#{search_string}%"])
+      find(:all, :conditions => ['tag ILIKE UPPER(?)', "%#{search}%"])
     end
   end
   
