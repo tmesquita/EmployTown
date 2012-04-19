f256e748ef24282b7f710056e4556da81791b0a3
Jerel Miller <jermille@mines.edu>
2012-04-19 05:13:06 -0600

    Added search on email as well

--- app/controllers/employers/search_controller.rb
+++ app/controllers/employers/search_controller.rb
@@ -1,7 +1,6 @@
 class Employers::SearchController < Employers::EmployersController
   def index
   	unless params[:search].blank?
-  		puts params[:search]
   		@users = User.search(params[:search])
   		@users.delete_if{|user| user.seeking.eql? "employer"}
   		@tags = Tag.search(params[:search]).remove_duplicates_by{|tag| tag.user_id}
