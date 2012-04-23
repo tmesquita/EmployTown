e91643d6486c3413c9c0cb83d753e8421e6da21b
Jerel Miller <jermille@mines.edu>
2012-04-23 04:20:16 -0600

    Fixed bug on blank about me

--- app/helpers/biddings_helper.rb
+++ app/helpers/biddings_helper.rb
@@ -1,7 +1,7 @@
 module BiddingsHelper
 
   def more_link(h)
-    if h.comment.length > 200
+    if !h.comment.nil? and h.comment.length > 200
       link_to_function("Read More", nil, :id => "1") do |page|
         page["bid#{h.id}"].replace_html "#{simple_format(h.comment)}"
         page.insert_html :after, "more#{h.id}", "<div class='less' id='less#{h.id}'>#{less_link(h)}</div>"
