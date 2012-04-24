8ac2723e936cdf6ebae3871d1c0f28f5a779e683
Jerel Miller <jermille@mines.edu>
2012-04-23 23:09:52 -0600

    Worked on form stuff for login and new user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,12 @@
 class SessionsController < ApplicationController
   def create
     user = login(params[:email], params[:password], params[:remember_me])
-      if user
-        redirect_back_or_to home_url_for(user)
-      else
-        flash[:error] = "Email or password was invalid"
-        render :new
-      end
+    if user
+      redirect_back_or_to home_url_for(user)
+    else
+      flash[:error] = "Email or password was invalid"
+      render :new
+    end
   end
   
   def destroy
