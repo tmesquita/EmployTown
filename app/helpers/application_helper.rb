8799608a3bba3d525e0b4c09e1df13b932c6635f
Jerel Miller <jermille@mines.edu>
2012-08-22 23:21:52 -0600

    Some header changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,29 +1,5 @@
 module ApplicationHelper
 
-def general_nav(user)
-  if user
-    if user.is_job_seeker?
-      render :partial => "layouts/seekernav"
-    elsif user.is_employer?
-      render :partial => "layouts/employernav"
-    end
-  else
-    render :partial => "layouts/guestnav"
-  end
-end
-
-# def quick_nav(user)
-#   if user
-#     if user.is_job_seeker?
-#       render :partial => "layouts/seekerquicknav"
-#     elsif user.is_employer?
-#       render :partial => "layouts/employerquicknav"
-#     end
-#   else
-#     render :partial => "layouts/guestquicknav"
-#   end
-# end
-
 def search(user)
   if user
     if user.is_employer?
@@ -37,10 +13,5 @@ def root_path_for(user)
   return employers_root_path if user.is_employer?
   root_path
 end
-
-# def flash_heading(flash)
-#   return 'Success' if flash[:success]
-#   return 'Error!' if flash[:error]
-# end
 
 end
